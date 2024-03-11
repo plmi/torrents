91b99511687465c6e766d65cbdf9eec70fbeb886
plmi <65455617+plmi@users.noreply.github.com>
2024-03-11 23:38:08 +0100

    Add script: auto-prepare.sh

---
+++ auto-prepare.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# usage ./<program> <source directory> <mount point> <destination directory>
+
+function log() {
+  echo -e "\e[1m[\e[32m+\e[0m\e[1m]\e[0m ${1}"
+}
+
+SOURCE_DIRECTORY="$1"
+
+# remove Sample and Proof directory
+log "Strip Sample and Proof directory"
+find "$SOURCE_DIRECTORY" -type d \( -iname 'Proof' -o -iname 'Sample' \) -exec rm -r {} + && \
+
+# search for folders that match a dirname and trim {{.*}}
+log "Sanitize directory names"
+find "$SOURCE_DIRECTORY" -maxdepth 1 -type d -regex '^\.\/.*-.+{{.*?}}$' -execdir sh -c 'mv "$1" "$(echo "$1" | sed "s/{{.*}}$//")"' sh {} \;
+
+# create mediainfo for each directory
+log "Run mediainfo"
+MOUNTPOINT="$2"
+find "$SOURCE_DIRECTORY" -type f \( -iname "*.mkv" -o -iname "*.iso" \) | while read -r file; do
+  OUTPUT_JSON="$(basename "$(dirname "$(readlink -f "${file}")")").json"
+  if [[ $file == *mkv ]]; then
+    mediainfo --Output=JSON "$file" > "$OUTPUT_JSON"
+  elif [[ $file == *iso ]]; then
+    # images need to be mounted first
+    sudo umount "$MOUNTPOINT"
+    sudo mount "$file" "$MOUNTPOINT" -o loop,ro && \
+    # create mediainfo from largest file in /BDMV/STREAM
+    largest_file=$(find "${MOUNTPOINT}/BDMV/STREAM/" -type f -exec du -a {} + | sort -nr | head -n 1 | cut -f2) && \
+    mediainfo --Output=JSON "$largest_file" > "$OUTPUT_JSON" && \
+    sudo umount "$MOUNTPOINT"
+  fi
+done
+
+# create torrent files
+log "Create torrent files"
+find "$SOURCE_DIRECTORY" -maxdepth 1 -type d -regex '^\.\/.*-.+$' | xargs -I {} \
+  maketorrent --announce "https://torrent-syndikat.org/ts_ann.php" --piece-length 24 \
+  --private --name "$(basename {})" {}
+
+DESTINATION_PATH="$3"
+# move folders to destination
+log "Move files to destination"
+find "$SOURCE_DIRECTORY" -maxdepth 1 -type d -regex '^(\w\.?)+-.+' -exec mv -t "${DESTINATION_PATH}" {} +
