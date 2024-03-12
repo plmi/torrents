5c7b7954e571d4c0659e9d1ff0b2177b3ef75ab9
plmi <65455617+plmi@users.noreply.github.com>
2024-03-12 22:57:04 +0100

    Upadte auto-prepare.sh

--- auto-prepare.sh
+++ auto-prepare.sh
@@ -11,6 +11,7 @@ SOURCE_DIRECTORY="$1"
 # remove Sample and Proof directory
 log "Strip Sample and Proof directory"
 find "$SOURCE_DIRECTORY" -type d \( -iname 'Proof' -o -iname 'Sample' \) -exec rm -r {} + && \
+find "$SOURCE_DIRECTORY" -type f \( -iname '*.proof.*' -o -iname '*.sample.*' -o iname '*mediainfo.nfo' -o -iname "*usenet*" \) -exec rm -r {} + && \
 
 # search for folders that match a dirname and trim {{.*}}
 log "Sanitize directory names"
@@ -23,17 +24,18 @@ find "$SOURCE_DIRECTORY" -mindepth 2 -maxdepth 2 -type d -empty -delete && \
 # create mediainfo for each directory
 log "Run mediainfo"
 MOUNTPOINT="$2"
+DESTINATION_PATH="$3"
 find "$SOURCE_DIRECTORY" -type f \( -iname "*.mkv" -o -iname "*.iso" \) | while read -r file; do
   OUTPUT_JSON="$(basename "$(dirname "$(readlink -f "${file}")")").json"
   if [[ $file == *mkv ]]; then
-    mediainfo --Output=JSON "$file" > "$OUTPUT_JSON"
+    mediainfo --Output=JSON "$file" > "${DESTINATION_PATH}/$OUTPUT_JSON"
   elif [[ $file == *iso ]]; then
     # images need to be mounted first
     sudo umount "$MOUNTPOINT"
     sudo mount "$file" "$MOUNTPOINT" -o loop,ro && \
     # create mediainfo from largest file in /BDMV/STREAM
     largest_file=$(find "${MOUNTPOINT}/BDMV/STREAM/" -type f -exec du -a {} + | sort -nr | head -n 1 | cut -f2) && \
-    mediainfo --Output=JSON "$largest_file" > "$OUTPUT_JSON" && \
+    mediainfo --Output=JSON "$largest_file" > "${DESTINATION_PATH}/$OUTPUT_JSON" && \
     sudo umount "$MOUNTPOINT"
   fi
 done
@@ -43,9 +45,8 @@ log "Create torrent files"
 ANNOUNCE_URL="$4"
 find "$SOURCE_DIRECTORY" -maxdepth 1 -type d -regex '.*-.+' | xargs -I {} \
   maketorrent --announce "${ANNOUNCE_URL}" --piece-length 24 \
-  --private --name "$(basename {})" {}
+  --private --name "${DESTINATION_PATH}/$(basename {})" {}
 
-DESTINATION_PATH="$3"
 # move folders to destination
 log "Move files to destination"
 find "$SOURCE_DIRECTORY" -maxdepth 1 -type d -regex '.*-.+' -exec mv -t "${DESTINATION_PATH}" {} +
