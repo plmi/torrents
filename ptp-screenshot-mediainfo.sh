7c63cbf951e5a8ef1b7d18fb97c59935c7ac1965
plmi <65455617+plmi@users.noreply.github.com>
2024-03-25 10:50:04 +0100

    Update ptp-screenshot-mediainfo.sh

--- ptp-screenshot-mediainfo.sh
+++ ptp-screenshot-mediainfo.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-INPUT_FILE="$1"
-OUTPUT_DIRECTORY="$(realpath $2)"
+# usage: <program> <movie path>
+# creates a (regular, not full) mediainfo file and 3 screenshots
+# in 5 minute intervals in the parent directory of the movie
+
+INPUT_FILE="$(realpath "$1")"
+OUTPUT_DIRECTORY="$(dirname "$INPUT_FILE")"
 
 # create media info
-mediainfo "$INPUT_FILE" > "${OUTPUT_DIRECTORY}/$(basename $INPUT_FILE).mediainfo" && \
+mediainfo "$INPUT_FILE" > "${OUTPUT_DIRECTORY}.mediainfo" && \
   # take 3 screenshots
-  ffmpeg -ss 00:05:00 -y -i "$INPUT_FILE" -c:v png -frames:v 1 -loglevel error "${OUTPUT_DIRECTORY}/screenshot-1.png" && \
-  ffmpeg -ss 00:10:00 -y -i "$INPUT_FILE" -c:v png -frames:v 1 -loglevel error "${OUTPUT_DIRECTORY}/screenshot-2.png" && \
-  ffmpeg -ss 00:15:00 -y -i "$INPUT_FILE" -c:v png -frames:v 1 -loglevel error "${OUTPUT_DIRECTORY}/screenshot-3.png"
+  ffmpeg -ss 00:05:00 -y -i "$INPUT_FILE" -c:v png -frames:v 1 "${OUTPUT_DIRECTORY}-screenshot-1.png" && \
+  ffmpeg -ss 00:10:00 -y -i "$INPUT_FILE" -c:v png -frames:v 1 "${OUTPUT_DIRECTORY}-screenshot-2.png" && \
+  ffmpeg -ss 00:15:00 -y -i "$INPUT_FILE" -c:v png -frames:v 1 "${OUTPUT_DIRECTORY}-screenshot-3.png"
